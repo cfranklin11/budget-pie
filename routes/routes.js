c93244f23879e84e2757022b6d3c364f3cdf0996
Craig Franklin <craigjfranklin@gmail.com>
2016-03-29 15:51:32 +1100

    clean up form

--- routes/routes.js
+++ routes/routes.js
@@ -5,13 +5,16 @@ var dataHelper;
 dataHelper = require('../middleware/data-helper.js');
 
 module.exports = function(app) {
-  app.param('budget', function(req, res, next, budget) {
-    req.budget = budget;
-    next();
-  });
-
   app.get('/', function(req, res, next) {
     res.render('index', {title: 'Budget Pie'});
+  });
+  app.post('/', function(req, res, next) {
+    var form;
+
+    form = req.body;
+
+    console.log(form);
+    res.redirect('/budgets');
   });
   app.get('/budgets', function(req, res, next) {
     res.render('budgets', {title: 'Budgets'});
