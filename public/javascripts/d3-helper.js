8f61f5d645dd0ab7c78afec9432fd4842599be56
Craig Franklin <craigjfranklin@gmail.com>
2016-04-04 21:13:44 +1000

    bar charts with labels

--- public/javascripts/d3-helper.js
+++ public/javascripts/d3-helper.js
@@ -8,32 +8,28 @@ bbApp.D3Helper = {
     var title, w, h, percentage, svg, outerR, innerR, centerX, centerY,
       percentageString, labelSize, outerColor, innerColor, labelColor;
 
-
-
     title = data.title;
     title = title.replace(/\s/g, '-');
     w = 90;
     h = 100;
     percentage = data.budgetPercent;
 
-    svg = d3.select('#' + title)
+    svg = d3.select('#' + title + '-1')
       .append('svg')
       .attr('width', w.toFixed() + '%')
       .attr('height', h.toFixed() + '%')
       .attr('viewBox', '0 0 ' + w + ' ' + h)
       .attr('preserveAspectRatio', 'xMinYMin meet');
 
-      console.log(svg);
-
-      outerR = h / 2;
-      innerR = outerR * percentage;
-      centerX = w / 2;
-      centerY = h / 2;
-      percentageString = (percentage * 100).toFixed(1);
-      labelSize = 1;
-      outerColor = '#9d2235';
-      innerColor = '#FEB01A';
-      labelColor = '#fafafa';
+    outerR = h / 2;
+    innerR = outerR * percentage;
+    centerX = w / 2;
+    centerY = h / 2;
+    percentageString = (percentage * 100).toFixed(1);
+    labelSize = 1;
+    outerColor = '#9d2235';
+    innerColor = '#FEB01A';
+    labelColor = '#fafafa';
 
     // Create background circle (total budget value)
     svg.append('circle')
@@ -56,5 +52,81 @@ bbApp.D3Helper = {
       .attr('font-size', labelSize.toFixed() + 'em')
       .attr('fill', labelColor)
       .text(percentageString + '%');
-  }
+  },
+  createBarChart: function ( data ) {
+    var title, w, h, padding, labelSize, barColor, labelColor, years, count,
+      totalLastYear, totalThisYear, minTotal, maxTotal, chartData, totalChange,
+      percentChange, barWidth, xScale, yRange, yScale, yAxisScale, svg, bars,
+      label1, label2, label3, label4;
+
+    w = 100;
+    h = 100;
+    padding = 12;
+    labelSize = 1;
+    barColor = '#000000';
+    labelColor = '#000000';
+    years = ['This Year (2015/2016)', 'Last Year (2014/2015)'];
+    count = years.length;
+    title = data.title;
+    title = title.replace(/\s/g, '-');
+    totalLastYear = +data.dollarsLastYear;
+    totalThisYear = +data.dollarsThisYear;
+    minTotal = Math.min(totalLastYear, totalThisYear);
+    maxTotal = Math.max(totalLastYear, totalThisYear);
+    chartData = [totalLastYear, totalThisYear];
+    totalChange = totalThisYear - totalLastYear;
+    percentChange = totalThisYear / totalLastYear - 1;
+
+    barWidth = ( w ) / count;
+
+    yScale = d3.scale.linear()
+      .domain([minTotal, maxTotal])
+      .range([((h - padding) * (minTotal / maxTotal) * 0.9), (h - padding) * 0.9]);
+    yAxisScale = d3.scale.linear()
+      .domain([0, maxTotal])
+      .range([h - padding, padding]);
+
+    svg = d3.select('#' + title + '-2')
+      .append('svg')
+      .attr('width', w.toFixed())
+      .attr('height', h.toFixed())
+      .attr('viewBox', '0 0 ' + w.toFixed() + ' ' + h.toFixed())
+      .attr('preserveAspectRatio', 'xMinYMin meet');
+
+    bars = svg.selectAll( 'rect' )
+      .data( chartData )
+      .enter()
+      .append( 'rect' );
+    bars.attr( 'x', function ( d, i ) { return ( i * barWidth ); })
+      .attr( 'y', function ( d ) {return h - yScale( d ) - padding; })
+      .attr( 'width', barWidth )
+      .attr( 'height', function ( d ) {return (yScale(d) - padding);})
+      .attr( 'fill', '#2DB492' );
+
+    label1 = svg.append('text')
+      .attr('x', barWidth / 2)
+      .attr('y', h - padding)
+      .attr('text-anchor', 'middle')
+      .attr('font-size', padding)
+      .text('Last Year');
+    label2 = svg.append('text')
+      .attr('x', barWidth + barWidth / 2)
+      .attr('y', h - padding)
+      .attr('text-anchor', 'middle')
+      .attr('font-size', padding)
+      .text('This Year');
+
+    label3 = svg.append('text')
+      .attr('x', barWidth / 2)
+      .attr('y', h - yScale(totalLastYear) - padding + (yScale(totalLastYear) - padding) * 0.5)
+      .attr('text-anchor', 'middle')
+      .attr('font-size', padding)
+      .text('$' + totalLastYear);
+    label4 = svg.append('text')
+      .attr('x', barWidth + barWidth / 2)
+      .attr('y', h - yScale(totalThisYear) - padding + (yScale(totalThisYear) - padding) * 0.5)
+      .attr('text-anchor', 'middle')
+      .attr('font-size', padding)
+      .text('$' + totalThisYear);
+    }
 };
