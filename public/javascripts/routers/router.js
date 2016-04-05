af01c00f922fe151a010c7bdbed5b3910a2d62d2
Craig Franklin <craigjfranklin@gmail.com>
2016-04-05 23:31:45 +1000

    add more results event and functionality

--- public/javascripts/routers/router.js
+++ public/javascripts/routers/router.js
@@ -6,6 +6,8 @@ var bbApp = bbApp || {};
   var AppRouter = Backbone.Router.extend({
     initialize: function() {
       var self = this;
+
+      // Form submit event
       $('form').submit(function(event) {
         var inputs, values;
 
@@ -20,6 +22,26 @@ var bbApp = bbApp || {};
         });
 
         self.getBudgets(values);
+        $('#more-results').removeAttr('hidden');
+      });
+
+      // Get more results event
+      $('#more-results').click(function() {
+        var budgetsView, newMin, newMax, i;
+
+        budgetsView = window.budgetsView;
+        if (budgetsView) {
+          newMin = budgetsView.min + 4;
+          newMax = budgetsView.max + 4;
+
+          for (i = newMin; i < Math.min(newMax, budgetsView.collection.length); i++) {
+            budgetsView.addOne(budgetsView.collection.models[i], budgetsView);
+          }
+
+          if (newMax => budgetsView.collection.length) {
+            $('#more-results').attr('disabled', true);
+          }
+        }
       });
     },
     start: function () {
@@ -44,6 +66,7 @@ var bbApp = bbApp || {};
             clickRate: 0.02,
             clicks: 2,
             impressions: 100},
+
             {title: 'dummy program2',
             dollarsThisYear: 80,
             dollarsLastYear: 100,
@@ -52,6 +75,7 @@ var bbApp = bbApp || {};
             clickRate: 0.02,
             clicks: 2,
             impressions: 100},
+
             {title: 'dummy program3',
             dollarsThisYear: 80,
             dollarsLastYear: 100,
@@ -60,7 +84,26 @@ var bbApp = bbApp || {};
             clickRate: 0.02,
             clicks: 2,
             impressions: 100},
+
             {title: 'dummy program4',
+            dollarsThisYear: 80,
+            dollarsLastYear: 100,
+            budgetPercent: 0.2,
+            benefits: ['benefit1', 'benefit2', 'benefit3'],
+            clickRate: 0.02,
+            clicks: 2,
+            impressions: 100},
+
+            {title: 'dummy program5',
+            dollarsThisYear: 80,
+            dollarsLastYear: 100,
+            budgetPercent: 0.2,
+            benefits: ['benefit1', 'benefit2', 'benefit3'],
+            clickRate: 0.02,
+            clicks: 2,
+            impressions: 100},
+
+            {title: 'dummy program6',
             dollarsThisYear: 80,
             dollarsLastYear: 100,
             budgetPercent: 0.2,
@@ -72,7 +115,11 @@ var bbApp = bbApp || {};
 
 
           bbApp.budgets.reset(budgets);
-          self.budgetsView = new bbApp.BudgetsView({collection: bbApp.budgets});
+          self.budgetsView = new bbApp.BudgetsView({
+            collection: bbApp.budgets,
+            min: 0,
+            max: 4
+          });
 
           // for (i = 0; i < budgets.length; i++) {
           //   bbApp.D3Helper.createCircleChart(budgets[i]);
--- public/javascripts/views/budgets-view.js
+++ public/javascripts/views/budgets-view.js
@@ -7,20 +7,33 @@ var bbApp = bbApp || {};
     el: $('#results'),
     template: _.template(
       '<div id="data" class="container"></div>'),
-    initialize: function() {
+    initialize: function(options) {
       this.collection.on('add', this.addOne, this);
       this.collection.on('reset', this.render, this);
-      this.render();
+      this.render(options);
     },
-    render: function() {
+    render: function(options) {
+      var self, length, i;
+
+      console.log(this);
+
+      self = this;
+      this.min = options.min;
+      this.max = options.max;
+      length = this.collection.length;
       this.$el.html(this.template());
-      this.collection.each(this.addOne, this);
+
+      for (i = this.min; i < Math.min(this.max, length); i++) {
+       this.addOne(this.collection.models[i], self);
+      }
     },
-    addOne: function(budget) {
+    addOne: function(budget, self) {
       var budgetView;
 
+      console.log(budget);
+
       budgetView = new bbApp.BudgetView({model: budget});
-      this.$el.children('#data').append(budgetView.render().el);
+      self.$el.children('#data').append(budgetView.render().el);
     }
   });
 })(jQuery);
